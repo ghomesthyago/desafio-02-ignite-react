03f5bbfe580c4e90b94ea6b2ecb87705cd790f2a
Tiago Gomes <ghomes.thyago@gmail.com>
2021-04-27 09:10:52 -0300

    first commit

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,3 +1,34 @@
-export function Content() {
+import { MovieCard } from './MovieCard'
+
+interface IContentProps {
+  selectedGenre: { title: string };
+  movies: Array<{
+    imdbID: string;
+    Title: string;
+    Poster: string;
+    Ratings: Array<{
+      Source: string;
+      Value: string;
+    }>;
+    Runtime: string;
+  }>
+}
+
+export function Content({selectedGenre, movies}: IContentProps) {
   // Complete aqui
+  return(
+    <div className="container">
+      <header>
+        <span className="category">Categoria:<span> {selectedGenre.title}</span></span>
+      </header>
+
+      <main>
+        <div className="movies-list">
+          {movies.map(movie => (
+            <MovieCard key ={movie.imdbID} title={movie.Title} poster={movie.Poster} runtime={movie.Runtime} rating={movie.Ratings[0].Value} />
+          ))}
+        </div>
+      </main>
+    </div>
+  )
 }
